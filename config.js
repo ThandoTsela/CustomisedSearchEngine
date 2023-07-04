f3cc201b6c8c4c8f889811daa9cfcd465b6f4411
Thando Tsela <91032983+ThandoTsela@users.noreply.github.com>
2023-07-03 23:09:42 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+
+    'FIREBASE_KEY' : 'AIzaSyATKswTDdUM3O32dHrn5TaluSM0d4Vc7uU',
+    
+    'GOOGLE_KEY' : 'AIzaSyA7h7bFdIN9u5up5P32MHIdB7wacieRwC8',
+    
+   }
+
+//    function fbKey(){
+//     console.log(config['FIREBASE_KEY'])
+//     return config['FIREBASE_KEY']
+//    }
